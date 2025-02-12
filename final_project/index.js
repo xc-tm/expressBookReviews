11a1f0bafb12a12b04b5eec2ca32661513354830
xc-tm <xc.tm@keemail.me>
2025-02-12 10:29:33 +0530

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -11,9 +11,31 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+
+  if (!req.session || !req.session.accessToken) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  try {
+
+    const decoded = jwt.verify(req.session.accessToken, "your_secret_key");
+    req.user = decoded;
+    next();
+  } catch (error) {
+    req.session.destroy();
+    return res.status(401).json({ error: "Unauthorized" });
+  }
 });
- 
+
+const arr = [];
+const user = "john";
+const pass = "pass";
+const user1 = "john1";
+const pass1 = "pass1";
+arr.push({user, pass});
+arr.push({user1, pass1});
+
+
 const PORT =5000;
 
 app.use("/customer", customer_routes);
